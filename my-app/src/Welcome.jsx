57d1ad0f7c438dabe34bd2bc970fb06352b23830
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-06 18:03:14 +0200

    refactoring es-2

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
+const name = "Alessandro";
 export class Welcome extends React.Component {
   render() {
-    return <p className="text-slate-800 font-semibold">Hello {this.props.name}</p>;
+    return <p className="text-slate-800 font-semibold">Hello {name}</p>;
   }
 }
