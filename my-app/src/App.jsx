185d38cf0bddb884d17448b8673faf286e324742
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-07 17:10:55 +0200

    feature: ex-13 of React done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Counter } from "./Counter";
 
 export class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Counter />
+      </div>
+    );
   }
 }
---
+++ my-app/src/Counter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export class Counter extends React.Component {
+  state = {
+    count: this.props.intialVal,
+  };
+  constructor(props) {
+    super(props);
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + this.props.increment,
+        };
+      });
+    }, this.props.timeout);
+  }
+  render(){
+    return <h1>Count: {this.state.count}</h1>
+  }
+}
+
+Counter.defaultProps = {
+  intialVal: 0,
+  increment: 1,
+  timeout: 1000,
+};
