e2d3e1e17ae4540fa9dcde4f3636eb7489d21e77
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-07 16:31:32 +0200

    refactoring: es-8 of React

--- my-app/src/Age.jsx
+++ my-app/src/Age.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export class Age extends React.Component {
   render() {
-    return <p>{this.props.age && `Your age is : ${this.props.age}`}</p>;
+    return <p> Your age is : 25</p>;
   }
 }
--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Age } from "./Age";
-export class Welcome extends React.Component{
-    render(){
-        return <div>
-            <p>Hello {this.props.name} !</p>
-            <Age age={25} />
-        </div>
-    }
+export class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello {this.props.name} !</p>
+        <p>{this.props.age && <Age/>}</p>
+      </div>
+    );
+  }
 }
