50e83fb9a6f6ab42ebcaeee17ce8c00e00871cb6
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-07 11:05:14 +0200

    feature: done es-11 of React

---
+++ my-app/src/Age.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Age extends React.Component {
+  render() {
+    return (
+      <p>
+        {this.props.age > 18
+          ? `Your age is ${this.props.age}`
+          : `You're too young`}
+      </p>
+    );
+  }
+}
