c8b9358f15e9bcf731008a61d6290fa4887c3d07
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-11 10:50:25 +0200

    refactoring es-2

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export class Welcome extends React.Component {
   render() {
-    return <p className="text-slate-800 font-semibold">Hello {name}</p>;
+    return <p className="text-slate-800 font-semibold">Hello {this.props.name}</p>;
   }
 }
