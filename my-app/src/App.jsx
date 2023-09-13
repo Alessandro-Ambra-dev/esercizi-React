3d99546ecb73fea484abd628d4d6122f6998e3db
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-13 18:55:43 +0200

    feature: done es-19 of React

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Login } from "./Login";
 
 export class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Login
+          onLogin={() => {
+            alert("Funziono Bene !!");
+          }}
+        />
+      </div>
+    );
   }
 }
---
+++ my-app/src/Login.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  handleInputs = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInputs}
+        />
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleInputs}
+        />
+        <button
+          type="button"
+          disabled={
+            this.state.username === "" && this.state.password === ""
+              ? true
+              : false
+          }
+          onClick={this.props.onLogin}
+        > Click me</button>
+      </div>
+    );
+  }
+}
