8714006b69ff477b1f4cebb88e788e5854ac09ae
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-16 19:26:26 +0200

    feature: done es-28 of React

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { LanguagueContext } from "./LenguageContext";
+import { DisplayLanguage } from "./DisplayLanguage";
 
 export class App extends React.Component {
+  state = {
+    language: "en",
+  };
+
+  handleLanguageChange = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <select name="" id value={this.state.language} onChange={this.handleLanguageChange}>
+          <option value="en">English</option>
+          <option value="it">Italiano</option>
+          <option value="dt">Deutch</option>
+        </select>
+        <LanguagueContext.Provider value={this.state.language}>
+          <DisplayLanguage />
+        </LanguagueContext.Provider>
+      </div>
+    );
   }
 }
---
+++ my-app/src/DisplayLanguage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LanguagueContext } from "./LenguageContext";
+export class DisplayLanguage extends React.Component{
+    render(){
+
+        return (
+          <div>
+          </div>
+        );
+    }
+}
