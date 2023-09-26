ab13dffc5cef156f98a6abf757d9868f53510c37
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-26 18:08:14 +0200

    feature: added es-40 of React

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { CarDetails } from "./CarDetails";
 
 export function App() {
-  return <div></div>;
+  return <div>
+    <CarDetails initialStatus={{model: "Toyota Yaris", prodYear: "2020", color:"violet"}}/>
+  </div>;
 }
---
+++ my-app/src/CarDetails.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+
+export function CarDetails({initialStatus}) {
+  const initialData = useRef('');
+
+  useEffect(()=>{
+    initialData.current.model.value = initialStatus.model
+    initialData.current.prodYear.value = initialStatus.prodYear
+    initialData.current.color.value = initialStatus.color
+  })
+
+  return (
+    <div>
+      <form ref={initialData} >
+        <input type="text" name="model"  />
+        <input type="text" name="prodYear" />
+        <input type="text" name="color"/>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
