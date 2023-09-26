6c48c46606f283daaae5d1f30923b214fdca195d
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-09-26 19:28:30 +0200

    feature: added es-43 of React

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { FilteredList } from "./FilteredList";
 
 export function App() {
-  return <div></div>;
+  return (
+    <div>
+      <FilteredList
+        persons={[
+          { name: "Alessandro", age: 25, id: 1 },
+          { name: "Vincenzo", age: 27, id: 2 },
+          { name: "Antonio", age: 22, id: 3 },
+          { name: "Caio", age: 17, id: 4 },
+        ]}
+      />
+    </div>
+  );
 }
