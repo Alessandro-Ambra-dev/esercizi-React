be37d7076c99e31a8f698a9d23cc8abcca610984
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-10-03 21:06:02 +0200

    feature: added es-52 of React

--- my-app/src/GithubUser.jsx
+++ my-app/src/GithubUser.jsx
@@ -1,10 +1,13 @@
 import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser(username) {
-  const { data, error, isLoading } = useGithubUser((username='Alessandro-Ambra-dev'));
+  const { data, error, isLoading, onReset } = useGithubUser(
+    (username = "Alessandro-Ambra-dev")
+  );
 
   return (
     <div>
+      <button onClick={onReset}>Reset the data</button>
       {error && <h3>There was an error</h3>}
       {isLoading && <h3>Loading...</h3>}
       {data && (
--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,13 +1,17 @@
 import useSWR from "swr";
 
 export function useGithubUser(username) {
-  const { data, error } = useSWR(
+  const { data, error, mutate } = useSWR(
     username !== null && `https://api.github.com/users/${username}`
   );
+  function handleResetUser() {
+    mutate();
+  }
 
   return {
     data,
     error,
     isLoading: !data && !error,
+    onReset: handleResetUser,
   };
 }
